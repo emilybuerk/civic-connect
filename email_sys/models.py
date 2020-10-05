3e636ca97161b2462a306a333869507569d7300a
Jennah Whitehill <jnw2aw@virginia.edu>
2020-10-05 17:06:30 -0400

    Template progress

--- email_sys/models.py
+++ email_sys/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+from django.db import models
+import datetime
+from django.utils import timezone
+
+
+class Template(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    def __str__(self):
+        return self.title
--- email_sys/views.py
+++ email_sys/views.py
@@ -1,3 +1,45 @@
+
+
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
+from django.views import generic
+from .models import Template
+from .forms import ScratchForm, TemplateForm
+
 
 # Create your views here.
+def prompt(request):
+    context = {'templates_list': Template.objects.all()}
+    return render(request, 'email_sys/prompt.html', context)
+
+def scratch(request):
+    if request.method == "POST":
+        form = ScratchForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.save()
+            return redirect('/preview')
+    else:
+        form = ScratchForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'email_sys/scratch.html', context)
+
+
+def from_template(request, template_id):
+    template = get_object_or_404(Template, pk=template_id)
+    if request.method == "POST":
+        form = TemplateForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.save()
+            return redirect('/preview')
+    else:
+        form = TemplateForm(name_placeholder=template.title, body_placeholder=template.body)
+        context = {
+            'form': form,
+        }
+        return render(request, 'email_sys/scratch.html', context)
