063ba337c509b6a65fb5a5408db4493c928d0981
Jennah Whitehill <jnw2aw@virginia.edu>
2020-10-06 08:57:58 -0400

    new files

---
+++ email_sys/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'email_sys'
+urlpatterns = [
+    path('prompt/', views.prompt, name='prompt_page'),
+    path('scratch/',views.scratch, name='scratch_page'),
+    path('<int:template_id>/template/', views.from_template, name='template_edit_page')
+
+]
