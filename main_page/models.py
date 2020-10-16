b3005a32fb0f8cf83ab1f1103ca61ea21fbacbd4
Kalman Buterbaugh <kb2cj@virginia.edu>
2020-10-16 16:12:05 -0400

    add resource models

--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
 
-# Create your models here.
+# Source for learning more about models: https://docs.djangoproject.com/en/3.1/topics/db/models/
+
+
+class Issue(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class Resource(models.Model):
+    STATUS_CHOICES = [
+        ('R', 'Rejected'),
+        ('P', 'Pending Approval'),
+        ('A', 'Approved')
+    ]
+    title = models.CharField(max_length=75)
+    url = models.CharField(max_length=100)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    submitter = models.ForeignKey(auth_models.User, on_delete=models.CASCADE)
