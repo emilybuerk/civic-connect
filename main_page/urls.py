7a9a42c7c21a192b70a053b5b7661b7e31dbd3dd
Kalman Buterbaugh <kb2cj@virginia.edu>
2020-10-16 17:10:27 -0400

    fix typo in urls/py

--- main_page/urls.py
+++ main_page/urls.py
@@ -6,6 +6,6 @@ app_name = 'main_page'#namespace (is this for the templates??)
 urlpatterns = [
     path('', views.HomeView.as_view(), name = 'home_page'),
     path('login/', views.LoginView.as_view(), name = 'login_page'),
-    path('email/', views.email, name = 'email_page')
+    path('email/', views.email, name = 'email_page'),
     path('resources/', views.ResourceView.as_view(), name = 'resource_page')
 ]
