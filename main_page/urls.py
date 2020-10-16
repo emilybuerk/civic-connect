d27df6d857f6d63246def6b8e10b8f633affe3fb
Kalman Buterbaugh <kb2cj@virginia.edu>
2020-10-16 17:01:16 -0400

    create resource page

--- main_page/urls.py
+++ main_page/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name = 'home_page'),
     path('login/', views.LoginView.as_view(), name = 'login_page'),
     path('email/', views.email, name = 'email_page')
+    path('resources/', views.ResourceView.as_view(), name = 'resource_page')
 ]
