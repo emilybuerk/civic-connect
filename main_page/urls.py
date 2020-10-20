72817cc8ee6b5b4c3c6268304b74f61290947856
Kalman Buterbaugh <kb2cj@virginia.edu>
2020-10-20 16:22:18 -0400

    add resource submission page

--- main_page/urls.py
+++ main_page/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home_page'),
     path('login/', views.LoginView.as_view(), name='login_page'),
     path('email/', views.email, name='email_page'),
-    path('resources/', views.ResourceView.as_view(), name='resource_page')
+    path('resources/', views.ResourceView.as_view(), name='resource_page'),
+    path('resources/submit', views.ResourceSubmitView.as_view(), name='resource_submit_page')
 ]
