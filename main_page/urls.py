f508117765787e01326f330c87ed4843b6e9cee4
Kalman Buterbaugh <kb2cj@virginia.edu>
2020-10-20 16:23:53 -0400

    typo in urls

--- main_page/urls.py
+++ main_page/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login_page'),
     path('email/', views.email, name='email_page'),
     path('resources/', views.ResourceView.as_view(), name='resource_page'),
-    path('resources/submit', views.ResourceSubmitView.as_view(), name='resource_submit_page')
+    path('resources/submit/', views.ResourceSubmitView.as_view(), name='resource_submit_page')
 ]
