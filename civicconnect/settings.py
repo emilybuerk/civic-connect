f9bfb21c5d2b18820e8734f1ee1d2381cf5fca73
adrielk <theadrielkim@gmail.com>
2020-10-24 16:44:48 -0400

    email template model

--- civicconnect/settings.py
+++ civicconnect/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'email_sys.apps.EmailSysConfig',
     'main_page.apps.MainPageConfig', #criticial for finding the templates
     'django.contrib.admin',
     'django.contrib.auth',
--- email_sys/models.py
+++ email_sys/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class EmailTemplate(models.Model):
+    template_text = models.TextField()
+    author = models.CharField(max_length = 200)
+    rating = models.IntegerField()
+    approval = models.BooleanField()
+    pub_date = models.DateTimeField('date published')
+
+
+
+
