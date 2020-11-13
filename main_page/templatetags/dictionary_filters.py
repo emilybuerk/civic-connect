2a9ec8938f0b22975d2a6e6e89b3b1945b9466f8
Kalman Buterbaugh <kb2cj@virginia.edu>
2020-11-13 15:51:22 -0500

    fix error

--- main_page/templatetags/dictionary_filters.py
+++ main_page/templatetags/dictionary_filters.py
@@ -3,9 +3,7 @@ Sources for template filters
 https://stackoverflow.com/questions/8000022/django-template-how-to-look-up-a-dictionary-value-with-a-variable
 https://docs.djangoproject.com/en/dev/howto/custom-template-tags/#writing-custom-template-filters
 """
-from django.template.base import Library
-
-register = Library()
+from django.template.defaulttags import register
 
 
 @register.filter
