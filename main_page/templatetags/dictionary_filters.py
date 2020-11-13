47bb40df4341b0b140d5178d8da6c094223f5bba
Kalman Buterbaugh <kb2cj@virginia.edu>
2020-11-13 15:49:12 -0500

    load filters

--- main_page/templatetags/dictionary_filters.py
+++ main_page/templatetags/dictionary_filters.py
@@ -1,6 +1,7 @@
 """
-Source for template filters
+Sources for template filters
 https://stackoverflow.com/questions/8000022/django-template-how-to-look-up-a-dictionary-value-with-a-variable
+https://docs.djangoproject.com/en/dev/howto/custom-template-tags/#writing-custom-template-filters
 """
 from django.template.base import Library
 
@@ -9,4 +10,7 @@ register = Library()
 
 @register.filter
 def dict_value(dictionary, key):
-    return dictionary[key]
+    try:
+        return dictionary[key]
+    except KeyError:
+        return key
